50b68ffff3fbc3e224d3e49b2c924c10e48bb4d2
merab <merab.apio@gmail.com>
2026-07-31 13:27:49 +0300

    news page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
+import NewsPage from "./pages/landing/news-page/NewsPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/news" element={<NewsPage />} />
       </Routes>
     </Router>
   );
