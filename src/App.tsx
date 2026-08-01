888f8e42626c73507ea31414cea50816709f4d17
@Babirye366 <babiryemable442@gmail.com>
2026-08-01 10:02:47 +0300

    added the club layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
 import NewsPage from "./pages/news-page/NewsPage";
 import AboutUs from "./pages/aboutus/Aboutus";
+import ClubsPage from "./pages/clubs/ClubsPage";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/news" element={<NewsPage />} />
-         <Route path="/about" element={<AboutUs />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/clubs" element={<ClubsPage />} />
       </Routes>
     </Router>
   );
