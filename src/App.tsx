a0262c87efbe27962e74be443a22727a7599d76a
merab <merab.apio@gmail.com>
2026-08-04 14:21:11 +0300

    finance page

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import Tickets from "./pages/landing/tickets/TicketsLandingPage";
 import SuperAdminDashboard from './pages/superadmin/SuperAdminDashboard';
 import GeneralAdminDashboard from './pages/generaladmin/GeneralAdminDashboard';
 import ComplianceAdmin from './pages/generaladmin/compliance/ComplianceAdmin';
+import FinanceAdmin from "./pages/generaladmin/financeadmin/FinanceAdmin";
 // import Personalize from "./pages/personalize/Personalize";
 
 function App() {
@@ -48,6 +49,7 @@ function App() {
         {/* General Admin dashboard */}
         <Route path="/dashboard/general-admin" element={<GeneralAdminDashboard />} />
         <Route path="/dashboard/general-admin/compliance" element={<ComplianceAdmin />}/>
+        <Route path="/dashboard/general-admin/finance" element={<FinanceAdmin />} />
 
 
         {/* <Route path="/personalize" element={<Personalize />} /> */}
